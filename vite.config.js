2359b371038a20a65bc71a7537080028aeefad1e
Creativementor <muhammadhamza318@gmail.com>
2024-09-03 20:44:00 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/My-App/',  // Replace 'your-repo-name' with the name of your GitHub repository
+  base: '/your-repo-name/',  // Replace 'your-repo-name' with the name of your GitHub repository
   plugins: [react()],
 });
