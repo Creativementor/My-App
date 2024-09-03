7d05a6d892eade1a49a8185364e66d91ef193612
Creativementor <muhammadhamza318@gmail.com>
2024-09-03 19:16:46 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <>
-
+  
 
       <Header header={header} user={user} />
       <h1>Welcome to My Webpage</h1>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/My-App/',  // Replace 'your-repo-name' with the name of your GitHub repository
   plugins: [react()],
-  base: "/My-App/"
-})
+});
